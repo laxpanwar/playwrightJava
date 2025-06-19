9afcea3a489db6560b596042b47770ff032aa375
laxpanwar <laxpanwar11@gmail.com>
2025-06-19 18:05:20 +0530

    update code

--- src/main/java/pages/ProductPage.java
+++ src/main/java/pages/ProductPage.java
@@ -2,12 +2,14 @@ package pages;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.WaitForSelectorState;
 
 public class ProductPage {
     private Page page;
     //Locators
     private Locator addtocartbtn;
     private static Locator addedprompt;
+    private Locator continuebtn;
 
     public ProductPage(Page page) {
         this.page = page;
@@ -16,8 +18,9 @@ public class ProductPage {
     }
 
     private void initLocators() {
-        addtocartbtn = page.locator("(//a[@data-product-id= '1'])[1]");
+        addtocartbtn = page.locator("//a[@class= 'btn btn-default add-to-cart']");
         addedprompt = page.locator("(//p[@class= 'text-center'])[1]");
+        continuebtn = page.locator("//button[@class= 'btn btn-success close-modal btn-block']");
 
     }
 
@@ -26,11 +29,26 @@ public class ProductPage {
         addtocartbtn.click();
 
     }
-    public static String getAddedText(){
+
+    public static String getAddedText() {
         return addedprompt.textContent();
     }
 
+    public void addProductsToAddToCart() {
+        //int count = addtocartbtn.count();
 
+        for (int i = 0; i < 3; i++) {
+            Locator button = addtocartbtn.nth(i);
+            button.scrollIntoViewIfNeeded();
+            button.click(new Locator.ClickOptions().setForce(true));
+            System.out.println("Confirmation: " + addedprompt.textContent());
+            continuebtn.click();
+            //page.goBack(); // Navigate back to product list
+            //page.waitForLoadState(); // Wait for page to fully load again
+        }
+
+
+    }
 }
 
 
--- src/test/java/javatesting/ex01_Basics/productTest.java
+++ src/test/java/javatesting/ex01_Basics/productTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import pages.CategoriesPage;
 import pages.HomePage;
 import pages.LoginPage;
 import pages.ProductPage;
@@ -14,12 +15,15 @@ public class productTest extends BaseTest {
     private LoginPage loginPage;
     private HomePage homePage;
     private ProductPage productPage;
+    private CategoriesPage categoriesPage;
 
 
     @BeforeClass
     public void init() {
         setup();
         loginPage = new LoginPage(page);
+        productPage = new ProductPage(page);
+
     }
 
     @Test
@@ -27,9 +31,11 @@ public class productTest extends BaseTest {
         String baseUrl = ConfigReader.get("url");
         loginPage.navigateTo(baseUrl);
         homePage = loginPage.loginValidCredentials();
-        productPage = homePage.clickOnTheProductBtn();
-        Assert.assertEquals(ProductPage.getAddedText(),"Your product has been added to cart.");
-
+        //productPage = homePage.clickOnTheProductBtn();
+        //Assert.assertEquals(ProductPage.getAddedTex(),"Your product has been added to cart.");
+        categoriesPage = homePage.clickontheWomenBtn();
+        categoriesPage.clickontheTopsBtn();
+        productPage.addProductsToAddToCart();
     }
 
     @AfterClass
