c6c657a1cf263e668740e0d63465db50d67ac20d
laxpanwar <laxpanwar11@gmail.com>
2025-06-20 15:25:02 +0530

    added new methods, locators and update code

--- src/main/java/pages/ProductPage.java
+++ src/main/java/pages/ProductPage.java
@@ -10,6 +10,10 @@ public class ProductPage {
     private Locator addtocartbtn;
     private static Locator addedprompt;
     private Locator continuebtn;
+    private Locator itemsoptions;
+    private Locator addtoartoverlay;
+    private Locator cartbtn;
+    private Locator cartproductlist;
 
     public ProductPage(Page page) {
         this.page = page;
@@ -18,9 +22,13 @@ public class ProductPage {
     }
 
     private void initLocators() {
-        addtocartbtn = page.locator("//a[@class= 'btn btn-default add-to-cart']");
+        addtocartbtn = page.locator("//div[contains(@class, 'productinfo')]//a[contains(@class, 'add-to-cart')]");
+        addtoartoverlay= page.locator("//div[contains(@class, 'overlay-content')]//a[contains(@class, 'add-to-cart')]");
         addedprompt = page.locator("(//p[@class= 'text-center'])[1]");
         continuebtn = page.locator("//button[@class= 'btn btn-success close-modal btn-block']");
+        itemsoptions = page.locator("//div[@class= 'productinfo text-center']");
+        cartbtn= page.locator("(//a[@href='/view_cart'])[1]");
+        cartproductlist= page.locator("//td[@class='cart_product']");
 
     }
 
@@ -35,21 +43,30 @@ public class ProductPage {
     }
 
     public void addProductsToAddToCart() {
-        //int count = addtocartbtn.count();
-
-        for (int i = 0; i < 3; i++) {
-            Locator button = addtocartbtn.nth(i);
-            button.scrollIntoViewIfNeeded();
-            button.click(new Locator.ClickOptions().setForce(true));
-            System.out.println("Confirmation: " + addedprompt.textContent());
+        int total = itemsoptions.count();
+        System.out.println(total);
+        for (int i = 0; i < itemsoptions.count(); i++) {
+            Locator item = itemsoptions.nth(i);
+            addtocartbtn.nth(i).hover();
+            addtoartoverlay.nth(i).click();
+            //System.out.println("Confirmation: " + addedprompt.textContent());
             continuebtn.click();
-            //page.goBack(); // Navigate back to product list
-            //page.waitForLoadState(); // Wait for page to fully load again
+        }
+        page.evaluate("window.scrollTo(0, 0)");
+        cartbtn.waitFor(new Locator.WaitForOptions().setTimeout(5000));
+        cartbtn.click();
+        int totalproduct= cartproductlist.count();
+        System.out.println(totalproduct);
+        if (total == totalproduct) {
+            System.out.println("Product count matches item count.");
+        } else {
+            System.out.println("Mismatch: Products in cart = " + total + ", Expected = " + totalproduct);
         }
 
-
     }
+
 }
+
 
 
 
